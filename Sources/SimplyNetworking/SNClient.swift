80cdfb2e01289e4cf0e9f0d6e4664de86db55754
Lee Burrows <>
2019-11-20 13:19:04 +0000

    Access Modifiers

--- Sources/SimplyNetworking/SNClient.swift
+++ Sources/SimplyNetworking/SNClient.swift
@@ -10,6 +10,7 @@ import Foundation
 
 public struct SNClient: APIClientProtocol {
 
+    public init() { }
     public func request<T: Codable>(_ request: URLRequest, completion: @escaping (Result<T, NetworkLoaderError>) -> Void) {
 
         let task = URLSession.shared.dataTask(with: request) { data, _, error in
